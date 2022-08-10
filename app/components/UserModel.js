2a3997bc69b85ecd0e08b7002aa9f979b51148ec
mohit.gawande@globant.com <mohit.gawande@globant.com>
2022-08-10 18:27:20 +0530

    new instance is created of model without extend keyword

--- app/components/UserModel.js
+++ app/components/UserModel.js
@@ -1,7 +1,12 @@
 import Backbone from 'backbone'
 
-var UserModel= Backbone.Model.extend({
-  
-})
+var UserModel = new Backbone.Model(
+    {
+        
+            name: 'mohit',
+            age: 23
+        
+    }
+)
 
 export default UserModel
--- app/initialize.js
+++ app/initialize.js
@@ -1,15 +1,12 @@
 
 import App from 'components/App';
 import UserView from './components/UserView'
-import UserModel from './components/UserModel'
+import model from './components/UserModel'
 App.addRegions({
   list:"#list"
 })
 App.addInitializer( function(){
-    var model =new UserModel({
-      name:'mohit',
-      age:23
-    })
+
 var userview=new UserView({model})
 App.list.show(userview)
 
